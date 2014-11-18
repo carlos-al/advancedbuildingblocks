c554f8308f3db96492d4c7f3c777c6580b2914fd
Carlos Antolín Lucas <carlos.antolin.lucas@gmail.com>
2014-11-18 21:36:25 +0100

    finished bubblesort_by, which takes a block to use as the comparison 'function'

--- bubblesort.rb
+++ bubblesort.rb
@@ -6,3 +6,13 @@ def bubblesort(array)
 	end
 	array
 end
+
+def bubblesort_by(array)
+	array.length.times do |i|
+		0.upto(array.length-i-2) do |k|
+			array[k+1], array[k] = array[k], array[k+1] if (yield array[k], array[k+1]) < 1
+		end
+	end
+	array
+end
+
